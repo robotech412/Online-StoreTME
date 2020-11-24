f563f74b3a12115f0022720a57a1481af4adec1d
Denis Ariel Ake Lopez <dariel_504@hotmail.com>
2020-11-24 04:58:25 -0600

    acceso a panel de admin y secciones de este panel

--- src/app/vista/admin/admin-routing.module.ts
+++ src/app/vista/admin/admin-routing.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from '../about/about.component';
+import { HomeComponent } from '../home/home.component';
+import { LoginComponent } from '../login/login.component';
+import { LoginModule } from '../login/login.module';
 
 import { AdminComponent } from './admin.component';
 
-const routes: Routes = [{ path: 'Admin', component: AdminComponent }];
+//const routes: Routes = [{ path: 'Admin', component: AdminComponent }];
+
+const appRoutes: Routes = [
+  {
+    path: 'dashboard', component: AdminComponent, 
+    children: [
+      { path: 'home', component: HomeComponent },
+      { path: 'about', component: AboutComponent },
+    ]
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forChild(appRoutes)],
   exports: [RouterModule]
 })
 export class AdminRoutingModule { }
--- src/app/vista/admin/admin.module.ts
+++ src/app/vista/admin/admin.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { RouterModule, Routes } from '@angular/router';
+
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
-
+import { HomeComponent } from '../home/home.component';
+import { AboutComponent } from '../about/about.component';
+import { LoginModule } from '../login/login.module';
 
 @NgModule({
   declarations: [AdminComponent],
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
   ]
 })
 export class AdminModule { }
