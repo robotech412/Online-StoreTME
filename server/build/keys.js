a67ade832bc793b344e572ea4690894cc8fa3ba8
Dennis Ariel Ake Lopez <dariel_504@hotmail.com>
2020-12-16 04:22:10 -0600

    update remote heroku database

--- server/build/keys.js
+++ server/build/keys.js
@@ -2,9 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     database: {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'tme_tienda'
+        host: 'us-cdbr-east-02.cleardb.com',
+        user: 'baa36c84be12f1',
+        password: 'e5847d64',
+        database: 'heroku_33ba792923b702a'
     }
 };
+// mysql://baa36c84be12f1:e5847d64@us-cdbr-east-02.cleardb.com/heroku_33ba792923b702a?reconnect=true
